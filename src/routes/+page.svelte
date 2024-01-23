ea93f2713391e97b0229db8cd9d80bdbdaadc6b8
sambok23 <a24746440@gmail.com>
2024-01-24 03:09:04 +0900

    a

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core'
+  import Scence from './scene.svelte'
+</script>
+
+<div class="scene">
+  <Canvas>
+    <Scence />
+  </Canvas>
+</div>
+
+<style>
+  .scene {
+    position: absolute;
+    inset: 0;
+    background-color: hsl(200 10% 10%);
+  }
+</style>
